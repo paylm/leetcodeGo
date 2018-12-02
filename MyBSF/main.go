ede9c1c35d02cec614d9458c80d7ed4f3d344172
paylm <172749114@qq.com>
2018-12-02 22:39:52 +0800

    添加search路径，但有bug待处理

--- MyBSF/main.go
+++ MyBSF/main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+//路径节点
+type Node struct {
+	Val  string
+	Prev *Node
+}
+
+func NewNode(v string) *Node {
+	n := new(Node)
+	n.Val = v
+	return n
+}
+
+//返回子节点
+func addNextNode(n *Node, v string) *Node {
+	sNode := NewNode(v)
+	if n == nil {
+		return sNode
+	}
+	sNode.Prev = n
+
+	return sNode
+}
+
+func traverse(last *Node) {
+	if last == nil {
+		return
+	}
+	c := last
+	for {
+		if c == nil {
+			break
+		}
+		fmt.Printf("%s->", c.Val)
+		c = c.Prev
+	}
+	fmt.Println()
+}
+
 /**测试数据
  A { B C D }
  B { B1 B2 B3 }
@@ -57,9 +95,52 @@ func DFS(route map[string][]string, dst string) bool {
 	return false
 }
 
+//通过bfs 算法从route找到终点
+func DFSwithPath(route map[string][]string, dst string) (bool, *Node) {
+	fmt.Println("查找节点:", dst)
+	searchQueue := route["root"]
+	var zR *Node
+	if len(searchQueue) == 0 {
+		return false, zR
+	}
+	searched := make(map[string]bool)
+	for {
+		if len(searchQueue) < 1 {
+			break
+		}
+		path := searchQueue[0]
+		searchQueue = searchQueue[1:]
+		//fmt.Printf("current search point %s\n", path)
+		if path == dst {
+			fmt.Println("已找到节点:", dst)
+
+			return true, zR
+		}
+
+		for _, x := range route[path] {
+			//fmt.Println(x, searched[x])
+			if _, ok := searched[x]; !ok {
+				searchQueue = append(searchQueue, x)
+				zR = addNextNode(zR, path)
+			} //else {
+			//	fmt.Printf("%s has checked , skip\n", x)
+			//}
+			searched[x] = true
+		}
+		//fmt.Println("queue:", searchQueue)
+	}
+	return false, nil
+}
+
 func main() {
 	fmt.Println("vim-go")
 	r := createRouter()
 	fmt.Println(DFS(r, "Z"))
-	fmt.Println(DFS(r, "C2"))
+
+	ok, zR := DFSwithPath(r, "C2")
+	fmt.Println(ok, zR)
+	if ok {
+
+		traverse(zR)
+	}
 }
