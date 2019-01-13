0a5428b8ac45fcb3dc80f74b1df7b9f7b92601a0
paylm <172749114@qq.com>
2019-01-13 10:42:47 +0800

    hash test

--- MyCode/lehash.go
+++ MyCode/lehash.go
@@ -1,32 +1,51 @@
 package main
 
-import "fmt"
+/**https://leetcode-cn.com/problems/top-k-frequent-elements/
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
 
+示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+
+输入: nums = [1], k = 1
+输出: [1]
+说明：
+
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+
+
+未通过 oops!!!
+
+**/
 func TopKFrequent(nums []int, k int) []int {
 	m := make(map[int]int) //m 存在的 element 在 kArr 的索引
 	kArr := []*tkF{}
 	for _, v := range nums {
 
 		ik, ok := m[v]
-		fmt.Printf("ok:%v,v:%d\n", ok, v)
 		if !ok {
 			tk := NewtkF(v)
 			kArr = Insert(kArr, tk)
 			m[v] = len(kArr) - 1
 		} else {
-			fmt.Printf("kArr:%v, v:%d ik:%d\n", kArr, v, ik)
 			tk := kArr[ik]
 			tk.fq++
-			TkfperUp(kArr, ik)
+			//TkfperUp(kArr, ik)
 		}
 
 	}
 
+	BuildHeap(kArr)
+
 	res := []int{}
 	for i := 0; i < k; i++ {
-		tk, ok := Pop(kArr)
+		tk, ok, arr := Pop(kArr)
 		if ok {
-			res = append(res, tk.key)
+			kArr = arr
+			res = append(res, tk)
 		}
 	}
 	return res
@@ -52,18 +71,19 @@ func compare(t1, t2 *tkF) bool {
 
 func TkfperDown(arr []*tkF, i int) {
 
-	leftChild := i * 2
+	leftChild := i*2 + 1
 	k := i
 
 	if leftChild < len(arr) && compare(arr[leftChild], arr[i]) {
 		k = leftChild
 	}
 
-	if leftChild+1 < len(arr) && compare(arr[leftChild+1], arr[i]) {
+	if leftChild+1 < len(arr) && compare(arr[leftChild+1], arr[k]) {
 		k = leftChild + 1
 	}
 	if k != i {
 		//swapTkf it
+		//fmt.Printf("TkfperDown i:%d arr[i]:%v,arr[child]:%v\n", i, arr[i], arr[k])
 		swapTkf(arr[k], arr[i])
 		TkfperDown(arr, k)
 	}
@@ -75,17 +95,19 @@ func TkfperUp(arr []*tkF, i int) {
 	if parent < 0 {
 		return
 	}
+	//fmt.Printf("TkfperUp arr[i]:%v,arr[parent]:%v\n", arr[i], arr[parent])
 	if compare(arr[i], arr[parent]) {
 		//swapTkf it
 		swapTkf(arr[i], arr[parent])
+		//fmt.Printf("after TkfperUp arr[i]:%v,arr[parent]:%v\n", arr[i], arr[parent])
 		TkfperUp(arr, parent)
 	}
 }
 
 func swapTkf(t1, t2 *tkF) {
-	temp := t1
-	t1 = t2
-	t2 = temp
+	temp := *t1
+	*t1 = *t2
+	*t2 = temp
 }
 
 func Insert(arr []*tkF, t *tkF) []*tkF {
@@ -93,16 +115,45 @@ func Insert(arr []*tkF, t *tkF) []*tkF {
 	return arr
 }
 
-func Pop(arr []*tkF) (*tkF, bool) {
+func BuildHeap(arr []*tkF) {
+	l := len(arr)
+	for i := l / 2; i >= 0; i-- {
+		TkfperDown(arr, i)
+	}
+}
+
+func Pop(arr []*tkF) (int, bool, []*tkF) {
 	if len(arr) < 0 {
-		return nil, false
+		return 0, false, nil
 	}
 
-	res := arr[0]
+	res := arr[0].key
 	last := len(arr)
 	swapTkf(arr[0], arr[last-1])
 	arr = append(arr[:last-1])
 	TkfperDown(arr, 0)
 
-	return res, true
+	return res, true, arr
+}
+
+//测试工具方法,单元测试调用
+//set 简单比较
+func compareArr(arr1, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	//用hash 方法减少比较次数
+	m := make(map[int]bool)
+	for _, v := range arr1 {
+		m[v] = true
+	}
+
+	for _, v := range arr2 {
+		_, ok := m[v]
+		if !ok {
+			return false
+		}
+	}
+	return true
 }
--- MyCode/lehash_test.go
+++ MyCode/lehash_test.go
@@ -4,21 +4,29 @@ import (
 	"testing"
 )
 
-func Test_TopKFrequent(t *testing.T) {
-	t1 := []int{1, 1, 1, 2, 2, 3}
-	t1res := []int{1, 2}
-	res := TopKFrequent(t1, 2)
-
-	if len(res) != len(t1res) {
-		t.Errorf("test fail , %v not eq  res :%v\n", t1res, res)
-		t.Fail()
+func Test_TopKFrequent_1(t *testing.T) {
+	testData := []struct {
+		arr []int
+		k   int
+		ans []int
+	}{
+		{arr: []int{1, 1, 1, 2, 2, 3}, k: 2, ans: []int{1, 2}},
+		{arr: []int{3, 0, 0}, k: 1, ans: []int{0}},
+		{arr: []int{4, 1, -1, 2, -1, 2, 3}, k: 2, ans: []int{-1, 2}},
+		{arr: []int{2, 3, 4, 1, 4, 0, 4, -1, -2, -1}, k: 2, ans: []int{4, -1}},
 	}
+	for ti, data := range testData {
+		res := TopKFrequent(data.arr, data.k)
 
-	for i, v := range t1res {
-		if len(res) > i && res[i] == v {
-			t.Logf("test pass %d <=> %d \n", v, res[i])
+		if len(res) != len(data.ans) {
+			t.Errorf("%d test fail , %v not eq  res :%v\n", ti, data.ans, res)
+			t.Fail()
+		}
+
+		if compareArr(res, data.ans) {
+			t.Logf("test pass at %d, input:%v,output:%v,corret:%v\n", ti, data.arr, res, data.ans)
 		} else {
-			t.Errorf("test fail at %d <=> res:%d", v, res[i])
+			t.Errorf("test fail at %d, input:%v,output:%v,corret:%v\n", ti, data.arr, res, data.ans)
 		}
 	}
 }
