619b5f77ab59a6eb94972ad90050080af856b131
paylm <172749114@qq.com>
2019-02-19 15:43:53 +0800

    add UnionSet by size

--- MyDisjSet/DisjSet.go
+++ MyDisjSet/DisjSet.go
@@ -1,35 +1,43 @@
 package MyDisjSet
 
-
-type DisjSet struct{
-	S  []int
+type DisjSet struct {
+	S []int
 }
 
-func NewDisjSet(len int) *DisjSet{
+func NewDisjSet(len int) *DisjSet {
 	ds := new(DisjSet)
-	ds.S = make([]int,len)
-	for i:=0;i<len;i++{
+	ds.S = make([]int, len)
+	for i := 0; i < len; i++ {
 		ds.S[i] = -1
 		ds.S[i] = -1
 	}
 	return ds
 }
 
-func UnionSet(dj *DisjSet,root1 int,root2 int){
-	if dj.S[root1] > dj.S[root2]{
+//union by height
+func UnionSet(dj *DisjSet, root1 int, root2 int) {
+	if dj.S[root1] > dj.S[root2] {
 		dj.S[root1] = root2
-	}else{
-		if dj.S[root1] == dj.S[root2]{
+	} else {
+		if dj.S[root1] == dj.S[root2] {
 			dj.S[root1]--
 		}
 		dj.S[root2] = root1
 	}
 }
 
-func Find(dj *DisjSet,e int) int{
-	if dj.S[e]< 0 {
+func UnionSetBySize(dj *DisjSet, root1 int, root2 int) {
+	if root1 > root2 {
+		dj.S[root1] = root2
+	} else {
+		dj.S[root2] = root1
+	}
+}
+
+func Find(dj *DisjSet, e int) int {
+	if dj.S[e] < 0 {
 		return e
-	}else{
-		return Find(dj,dj.S[e])
+	} else {
+		return Find(dj, dj.S[e])
 	}
 }
--- MyDisjSet/DisjSet_test.go
+++ MyDisjSet/DisjSet_test.go
@@ -1,44 +1,57 @@
 package MyDisjSet
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestUnionSet(t *testing.T) {
 	dj := NewDisjSet(9)
-	UnionSet(dj,5,7)
-	UnionSet(dj,5,8)
-	UnionSet(dj,2,3)
+	UnionSet(dj, 5, 7)
+	UnionSet(dj, 5, 8)
+	UnionSet(dj, 2, 3)
 
 	if dj.S[5] != -2 {
-		t.Errorf("test fail , dj.S[%d] should be %d",5,-2)
+		t.Errorf("test fail , dj.S[%d] should be %d", 5, -2)
 	}
 	if dj.S[2] != -2 {
-		t.Errorf("test fail , dj.S[%d] should be %d",2,-2)
+		t.Errorf("test fail , dj.S[%d] should be %d", 2, -2)
 	}
 	//fmt.Println(dj.S)
-	UnionSet(dj,5,2)
+	UnionSet(dj, 5, 2)
 	if dj.S[5] != -3 || dj.S[2] != 5 {
 		t.Errorf("test fail at  UnionSet(dj,5,2) ")
 	}
 	fmt.Println(dj.S)
 }
 
+func TestUnionSetBySize(t *testing.T) {
+	dj := NewDisjSet(12)
+	UnionSetBySize(dj, 5, 7)
+	UnionSetBySize(dj, 5, 8)
+	UnionSetBySize(dj, 3, 5)
+	UnionSetBySize(dj, 3, 2)
+	UnionSetBySize(dj, 3, 1)
+	UnionSetBySize(dj, 2, 1)
+	UnionSetBySize(dj, 2, 0)
+	UnionSetBySize(dj, 5, 9)
+	fmt.Println(dj)
+}
+
 func TestFind(t *testing.T) {
 	dj := NewDisjSet(12)
-	UnionSet(dj,5,7)
-	UnionSet(dj,5,8)
-	UnionSet(dj,2,3)
-	UnionSet(dj,2,4)
-	UnionSet(dj,5,2)
-	UnionSet(dj,9,10)
-	if Find(dj,2) != Find(dj,8){
-		t.Errorf("test fail , %d %d belong same set",2,8)
+	UnionSet(dj, 5, 7)
+	UnionSet(dj, 5, 8)
+	UnionSet(dj, 2, 3)
+	UnionSet(dj, 2, 4)
+	UnionSet(dj, 5, 2)
+	UnionSet(dj, 9, 10)
+	if Find(dj, 2) != Find(dj, 8) {
+		t.Errorf("test fail , %d %d belong same set", 2, 8)
 	}
 
-	if Find(dj,1) == Find(dj,8){
-		t.Errorf("test fail , %d(%d) %d(%d) is not belong a same set",1,Find(dj,1),8,Find(dj,8))
+	if Find(dj, 1) == Find(dj, 8) {
+		t.Errorf("test fail , %d(%d) %d(%d) is not belong a same set", 1, Find(dj, 1), 8, Find(dj, 8))
 	}
 	fmt.Println(dj.S)
 }
