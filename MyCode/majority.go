5151789535a1176f12ac37e260e01a8c707c2489
paylm <172749114@qq.com>
2019-03-31 13:05:17 +0800

    Majority

---
+++ MyCode/majority.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+/**
+求众数
+数组出现次数大于为 n/2
+**/
+func majorityElement1(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	majority := nums[0]
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if count == 0 {
+			majority = nums[i]
+			count++
+			continue
+		}
+		if nums[i] == majority {
+			count++
+		} else {
+			count--
+		}
+	}
+	return majority
+}
+
+/**
+https://leetcode-cn.com/problems/majority-element-ii/
+求众数
+给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+
+示例 1:
+
+输入: [3,2,3]
+输出: [3]
+示例 2:
+
+输入: [1,1,1,3,3,2,2,2]
+输出: [1,2]
+
+还没完成
+**/
+func majorityElement(nums []int) []int {
+	//用摩尔投票法
+	ret := []int{}
+	var majority1, majority2 int
+	count1, count2 := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if count1 == 0 {
+			majority1 = nums[i]
+			count1++
+			continue
+		} else if count2 == 0 && nums[i] != majority1 {
+			majority2 = nums[i]
+			count2++
+			continue
+		}
+		if nums[i] == majority1 {
+			count1++
+			continue
+		} else {
+			count1--
+		}
+
+		if nums[i] == majority2 {
+			count2++
+		} else {
+			count2--
+		}
+	}
+	fmt.Printf("majority1:%d,count1:%d,majority2:%d,count2:%d\n", majority1, count1, majority2, count2)
+	if count1 > 0 {
+		ret = append(ret, majority1)
+	}
+	if count2 > 0 {
+		ret = append(ret, majority2)
+	}
+	return ret
+}
---
+++ MyCode/majority_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_MajorityElement1(t *testing.T) {
+	data := []struct {
+		input  []int
+		output int
+	}{
+		{input: []int{3, 2, 3}, output: 3},
+		{input: []int{1, 1, 1, 3, 3, 2, 1, 2}, output: 1},
+	}
+	for _, v := range data {
+		ret := majorityElement1(v.input)
+		if ret != v.output {
+			t.Errorf("test fail at %v ,return %v\n", v, ret)
+		}
+	}
+}
+func Test_MajorityElement(t *testing.T) {
+	data := []struct {
+		input  []int
+		output []int
+	}{
+		{input: []int{3, 2, 3}, output: []int{3}},
+		{input: []int{1, 1, 1, 3, 3, 2, 2, 2}, output: []int{1, 2}},
+		{input: []int{1, 1}, output: []int{1}},
+	}
+	for _, v := range data {
+		ret := majorityElement(v.input)
+		if len(ret) != len(v.output) {
+			t.Errorf("test fail at %v ,return %v\n", v, ret)
+		}
+	}
+}
