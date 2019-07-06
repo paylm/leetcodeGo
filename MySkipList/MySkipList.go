533e00203f580dbae655027abc98f75ba8d56c9f
paylm <172749114@qq.com>
2019-07-06 22:38:46 +0800

    skip table impl

---
+++ MySkipList/MySkipList.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	MAX_LEVEL = 64
+)
+
+type Data interface {
+	compare() bool
+}
+
+type ZskiplistNode struct {
+	obj      string
+	score    int
+	backward *ZskiplistNode
+	/**level     []struct {
+		forward *ZskiplistNode
+		span    int
+	}
+	**/
+	zkLevel []*ZskiplistNode //
+}
+
+type Zskiplist struct {
+	head  *ZskiplistNode
+	tail  *ZskiplistNode
+	level int //最大已用高度
+	len   int
+}
+
+func NewZskiplistNode(obj string, score int, level int) *ZskiplistNode {
+	zk := new(ZskiplistNode)
+	zk.obj = obj
+	zk.score = score
+	zk.zkLevel = make([]*ZskiplistNode, level)
+	return zk
+}
+
+func NewZskiplist() *Zskiplist {
+	zkl := new(Zskiplist)
+	zkl.head = NewZskiplistNode("", 0, MAX_LEVEL)
+	zkl.tail = NewZskiplistNode("", 0, MAX_LEVEL)
+	for i := 0; i < MAX_LEVEL; i++ {
+		zkl.head.zkLevel[i] = zkl.tail
+	}
+	zkl.tail.backward = zkl.head
+	return zkl
+}
+
+func randLevel() int {
+	l := 1
+	rand.Seed(time.Now().UnixNano())
+	for {
+		if rand.Intn(2) == 0 {
+			break
+		}
+		l++
+	}
+	if l < MAX_LEVEL {
+		return l
+	}
+	return MAX_LEVEL
+}
+
+func (zkl *Zskiplist) zslInsert(obj string, score int) error {
+	n := zkl.head
+	iLevel := randLevel()
+	update := make([]*ZskiplistNode, iLevel) //侍插入点的前指针
+	i := iLevel - 1
+	for {
+		if i < 0 {
+			break
+		}
+
+		if n.score == score && n != zkl.head {
+			return errors.New("arealdy exsit,insert fail")
+		}
+
+		if n.zkLevel[i] == zkl.tail || n.zkLevel[i].score > score {
+			//下沉
+			update[i] = n
+			i--
+		} else {
+			n = n.zkLevel[i]
+		}
+
+	}
+	//fmt.Printf("update:%v\n", update)
+	iN := NewZskiplistNode(obj, score, iLevel)
+	for j := iLevel - 1; j >= 0; j-- {
+		iN.zkLevel[j] = update[j].zkLevel[j]
+		update[j].zkLevel[j] = iN
+		iN.backward = update[j]
+	}
+	if iLevel > zkl.level {
+		zkl.level = iLevel
+	}
+	zkl.len++
+	return nil
+}
+
+func (zkl *Zskiplist) zslDelete(obj string, score int) bool {
+
+	c := zkl.head
+	i := zkl.level - 1
+	var delNode *ZskiplistNode
+	update := make([]*ZskiplistNode, zkl.level)
+	for {
+		if i < 0 {
+			break
+		}
+		if c.zkLevel[i] == zkl.tail || c.zkLevel[i].score >= score {
+			//下降一级查找
+			update[i] = c
+			if c.zkLevel[i].score == score {
+				delNode = c.zkLevel[i]
+			}
+			i--
+		} else {
+			c = c.zkLevel[i]
+		}
+	}
+
+	if delNode == nil || delNode == zkl.head || delNode == zkl.tail {
+		return true
+	}
+	//other level
+	for j := len(update) - 1; j > 0; j-- {
+		if len(delNode.zkLevel) > j {
+			update[j].zkLevel[j] = delNode.zkLevel[j]
+		}
+	}
+	//delte
+	delNode.zkLevel[0].backward = delNode.backward
+	delNode.backward.zkLevel[0] = delNode.zkLevel[0]
+
+	return true
+}
+
+func (zsk *Zskiplist) zslFind(score int) *ZskiplistNode {
+	c := zsk.head
+	i := zsk.level - 1
+	for {
+		if i < 0 {
+			break
+		}
+		if c.score == score && c != zsk.head {
+			return c
+		}
+		if c.zkLevel[i] == zsk.tail || c.zkLevel[i].score > score {
+			//下降一级查找
+			i--
+		} else {
+			c = c.zkLevel[i]
+		}
+	}
+	return nil
+}
+
+func (zsk *Zskiplist) show() {
+
+	c := zsk.head
+	for {
+		if c == nil {
+			break
+		}
+		//fmt.Printf("%d => %v\n", &(c), c)
+		fmt.Printf("%d \t %s\t", c.score, c.obj)
+		for i := 0; i < len(c.zkLevel); i++ {
+			if c.zkLevel[i] != nil && i <= zsk.level {
+				fmt.Printf(" %d\t", c.zkLevel[i].score)
+			}
+		}
+		fmt.Printf("\n")
+		c = c.zkLevel[0]
+	}
+}
+
+func (n *ZskiplistNode) show() {
+	fmt.Printf("%d \t %s \t", n.score, n.obj)
+	for i := 0; i < len(n.zkLevel); i++ {
+		fmt.Printf("%d\t", n.zkLevel[i].score)
+	}
+	fmt.Println()
+}
---
+++ MySkipList/MySkipList_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func Test_Insert(t *testing.T) {
+
+	zsl := NewZskiplist()
+
+	for i := 1; i < 30; i++ {
+		k := rand.Intn(100)
+		if err := zsl.zslInsert("sb", k); err != nil {
+			t.Logf("insert fail at %d\n", k)
+		}
+	}
+	zsl.show()
+	//if zsl.len != 11 {
+	//	t.Errorf("test fail , %v\n", zsl)
+	//} else {
+	//	t.Logf("test pass, zsl %v\n", zsl)
+	//}
+}
+
+func Test_Find(t *testing.T) {
+
+	zsl := NewZskiplist()
+
+	for i := 1; i < 1000; i++ {
+		k := rand.Intn(1000)
+		if err := zsl.zslInsert("sb", k); err != nil {
+			//	t.Logf("insert fail at %d\n", k)
+		}
+	}
+	zsl.zslInsert("test", 865)
+	//zsl.show()
+	if n := zsl.zslFind(865); n == nil {
+		t.Errorf("test fail , %v\n", zsl)
+	} else {
+		t.Logf("test pass, zsl %v , n =  %v\n", zsl, n)
+	}
+
+	if n := zsl.zslFind(1200); n == nil {
+		t.Logf("test pass , %v\n", zsl)
+	} else {
+		t.Errorf("test pass, zsl %v", zsl)
+	}
+}
+
+func Test_Delete(t *testing.T) {
+	data := []struct {
+		obj   string
+		score int
+	}{
+		{obj: "test7", score: 7},
+		{obj: "test118", score: 118},
+		{obj: "test1223", score: 1223},
+		{obj: "test0", score: 0},
+	}
+	zsl := NewZskiplist()
+
+	for i := 1; i < 1000; i++ {
+		k := rand.Intn(2000)
+		if err := zsl.zslInsert("sb", k); err != nil {
+			//	t.Logf("insert fail at %d\n", k)
+		}
+	}
+	for _, d := range data {
+		zsl.zslInsert(d.obj, d.score)
+
+		//zsl.show()
+		zsl.zslDelete(d.obj, d.score)
+
+		if n := zsl.zslFind(d.score); n != nil {
+			t.Errorf("test fail, %v should be delete \n", d)
+		}
+		//zsl.show()
+	}
+}
